9ca61c66133f50a79448944d73501a3954f4c0fa
Jianghan LI <helloworld11010111@gmail.com>
2017-02-13 07:22:18 +0100

    Add Q385

---
+++ 385.Mini_Parser/li_1line_jsonParse.js
@@ -0,0 +1,10 @@
+var deserialize = function(s) {
+    return JSON.parse(s);
+};
+
+// Well, Leetcode is supporting Javascript now
+// and sis a normal JSONstring.
+// Here is my json parse solution:
+
+// https://discuss.leetcode.com/topic/79157/
+
