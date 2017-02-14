9c60da53e899112fa1f1a03dba91d192271b268e
simei.yin <yinsimei>
2017-02-14 06:08:44 +0100

    add comment

--- 385.Mini_Parser/yin_recursive_n.cpp
+++ 385.Mini_Parser/yin_recursive_n.cpp
@@ -3,25 +3,25 @@ public:
     NestedInteger deserialize(string s)
     {
         // end condition
-        if (s.empty()) return NestedInteger(); // empty list
-        if (s[0] != '[') return NestedInteger(stoi(s)); // number value
+        if (s.empty()) return NestedInteger(); // empty string
+        if (s[0] != '[') return NestedInteger(stoi(s)); // number string
         
         // res
         NestedInteger res;
         for (int i = 1; i < s.size();)
         {
-            // value ni || list ni
+            // value ni OR list ni
 			if (isNumber(s[i]) || s[i] == '[')
 			{
-				int j = i;
+				int j = i; // end of substring for recursion
+				
 				if (isNumber(s[i])) // value ni
-					while (isNumber(s[j])) ++j;
+					while (isNumber(s[j])) ++j; // end of number string
 				
 				if (s[i] == '[') // list ni
 				{
-					// get pos of correspondent ']'
 					int cnt = 1;
-					while (cnt > 0) { ++j; cnt = s[j] == '[' ? cnt + 1 : s[j] == ']' ? cnt - 1 : cnt; }
+					while (cnt > 0) { ++j; cnt = s[j] == '[' ? cnt + 1 : s[j] == ']' ? cnt - 1 : cnt; } // pos of correspondent ']'
 				}
 				
 				res.add(deserialize(s.substr(i, j - i)));
